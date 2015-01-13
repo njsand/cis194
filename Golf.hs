1ec197c4c5a8c0b2ef2ab2bd660640b74a423988
njsand <njsand@internode.on.net>
2015-01-14 01:26:57 +1100

    werkin' on week 3 whatup

--- Golf.hs
+++ Golf.hs
@@ -12,6 +12,12 @@ skips xs = getZipList $ liftA2 takeNth (ZipList [1..]) (ZipList $ replicate (len
 
 -- Ex 2
 
+localMaxima :: [Integer] -> [Integer]
+localMaxima = id
 
+-- Group a list into triples yeah.
+triples :: [a] -> [(a,a,a)]
+triples xs = getZipList $ liftA3 (,,)
+               (ZipList xs) (ZipList $ drop 1 xs) (ZipList $ drop 2 xs)
 
 -- Ex 3

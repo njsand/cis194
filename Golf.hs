8cc07c7958df58bf7ce83f4c0e5a6642cad1db19
njsand <njsand@internode.on.net>
2015-01-17 00:55:00 +1100

    Did a pretty ugly week 3 ex 3

--- Golf.hs
+++ Golf.hs
@@ -4,6 +4,7 @@ module Golf where
 
 import Control.Applicative
 import ListHelper
+import Data.List
 
 -- Ex 1
 skips :: [a] -> [[a]]
@@ -14,3 +15,13 @@ localMaxima :: [Integer] -> [Integer]
 localMaxima = map (\(_, x, _) -> x) . filter (\(x,y,z) -> y > x && y > z) . triples
 
 -- Ex 3
+histogram :: [Integer] -> String
+histogram xs = let counts = map (\x -> length $ filter (== x) xs) [0..9]
+                   most = foldl1 max counts
+                   lines = map (\x -> map (display . (>= x)) counts) [most, most - 1 .. 0]
+               in concat . (intersperse "\n") $ lines ++ 
+                  [replicate 10 '='] ++ [concatMap show [0..9]] ++ [[]]
+
+display :: Bool -> Char
+display True = '*'
+display False = ' '

24398547ed4fd0ec9b2b439cfb6f01617acba0e8
njsand <njsand@internode.on.net>
2015-01-18 20:30:23 +1100

    Starting on week 4

---
+++ h4.hs
@@ -0,0 +1,18 @@
+-- Week 4 solutions
+
+import Data.List
+
+-- ex 1
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (subtract 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)

867683150f2dfe74e94d1cc70483a7838b3646da
njsand <njsand@internode.on.net>
2015-01-28 21:32:01 +1100

    Did ex 2 from week 4

--- h4.hs
+++ h4.hs
@@ -21,15 +21,37 @@ fun2 1 = 0
 fun2 n | even n = n + fun2 (n `div` 2)
        | otherwise = fun2 (3 * n + 1)
 
+-- Return next step in the hailstone sequence.
 step :: Integer -> Integer
 step n | even n = n `div` 2
        | otherwise = 3 * n + 1
 
--- hailstone again
+-- Sum the even numbers in the hailstone sequence starting at the given number.
 fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (/= 1) . iterate step
 
 -- ex 2
+-- |Binary tree
 data Tree a = Leaf
             | Node Integer (Tree a) a (Tree a)
               deriving (Show, Eq)
+
+-- |Create a balanced tree from a list of items.
+foldTree :: [a] -> Tree a
+foldTree = foldr (flip bInsert) Leaf
+
+bInsert :: Tree a -> a -> Tree a
+bInsert tree = fst . bInsert2 tree
+
+bInsert2 :: Tree a -> a -> (Tree a, Integer)
+bInsert2 Leaf x = (Node 0 Leaf x Leaf, 0)
+bInsert2 (Node _ l e r) x
+  | treeDepth l > treeDepth r = let (newTree, d) = bInsert2 r x in
+                                                   (Node (d + 1) l e newTree, d + 1)
+  | otherwise = let (newTree, d) = bInsert2 l x in
+                                   (Node (d + 1) newTree e r, d + 1)
+
+treeDepth :: Tree a -> Integer
+treeDepth Leaf = -1             -- A sentinel value
+treeDepth (Node d _ _ _) = d
+

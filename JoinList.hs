ddadafc18c44dcea9e94e30f85bc6708c46b8eac
njsand <njsand@internode.on.net>
2015-02-26 00:26:32 +1100

    More work on week 7

--- JoinList.hs
+++ JoinList.hs
@@ -9,6 +9,7 @@ data JoinList m a = Empty
                   | Append m (JoinList m a) (JoinList m a)
   deriving (Eq, Show)
 
+-- ex 1
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) l r = Append (tag l <> tag r) l r
 
@@ -18,6 +19,7 @@ tag Empty = mempty
 tag (Single m _) = m
 tag (Append m _ _) = m
 
+-- ex 2
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty = Nothing
 indexJ i _ | i < 0 = Nothing
@@ -26,5 +28,38 @@ indexJ i (Single _ y)
   | otherwise = Nothing
 indexJ i (Append m l r)
   | i >= (getSize $ size m) = Nothing
-  | i < (getSize $ size $ tag l) = indexJ i l
-  | otherwise = indexJ (i - (getSize $ size $ tag l)) r
+  | i < leftSize = indexJ i l
+  | otherwise = indexJ (i - leftSize) r
+  where leftSize = getSize $ size $ tag l
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ Empty = Empty
+dropJ 0 jl = jl
+dropJ _ (Single _ _) = Empty
+dropJ n jl@(Append m l r)
+  | n >= (getSize $ size m) = Empty
+  | n <= leftSize = dropJ n l +++ r
+  | n > leftSize = dropJ (n - leftSize) r
+  where leftSize = getSize $ size $ tag l
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ 0 _ = Empty
+takeJ _ s@(Single _ _) = s
+takeJ n a@(Append m l r)
+  | n >= (getSize $ size m) = a
+  | n <= leftSize = takeJ n l
+  | otherwise = l +++ takeJ (n - leftSize) r
+  where leftSize = (getSize $ size $ tag l)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+test = Append (Size 4)
+         (Append (Size 3)
+           (Single (Size 1) 'y')
+           (Append (Size 2)
+             (Single (Size 1) 'e')
+             (Single (Size 1) 'a')))
+         (Single (Size 1) 'h')
